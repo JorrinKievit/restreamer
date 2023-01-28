f9936bf0605668204e81b9e73eb1677ebf413cd7
Jorrin <jorrinkievit@hotmail.com>
2023-01-28 01:44:09 +0100

    Added continue watching

--- src/renderer/pages/SearchResultsPage.tsx
+++ src/renderer/pages/SearchResultsPage.tsx
@@ -1,8 +1,21 @@
 import { FC } from 'react';
-import { useParams } from 'react-router-dom';
-import { useSearchMoviesAndShows } from 'renderer/api/tmdb/api';
-import { Spinner } from '@chakra-ui/react';
-import SearchResults from 'renderer/components/SearchResults';
+import { Link, useParams } from 'react-router-dom';
+import {
+  TMDB_IMAGE_BASE_URL,
+  useSearchMoviesAndShows,
+} from 'renderer/api/tmdb/api';
+import {
+  AspectRatio,
+  Flex,
+  Grid,
+  GridItem,
+  Spinner,
+  Tag,
+  Tooltip,
+  VStack,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 
 const SearchResultsPage: FC = () => {
   const { query } = useParams();
@@ -11,11 +24,53 @@ const SearchResultsPage: FC = () => {
 
   if (isLoading) return <Spinner />;
 
-  if (error) {
-    return <div>{error.toString()}</div>;
-  }
+  if (error) return <div>{error.toString()}</div>;
 
-  return <SearchResults data={data} type="multi" />;
+  return (
+    <Grid
+      templateColumns={{
+        base: 'repeat(2, 1fr)',
+        md: 'repeat(6, 1fr)',
+      }}
+      gap={6}
+    >
+      {data?.results.map((show) => {
+        return (
+          show.poster_path && (
+            <GridItem key={show.id}>
+              <Link to={`/details/${show.id}?media_type=${show.media_type}`}>
+                <AspectRatio ratio={2 / 3}>
+                  <Image
+                    src={`${TMDB_IMAGE_BASE_URL}${show.poster_path}`}
+                    alt={show.name}
+                  />
+                </AspectRatio>
+                <VStack mt={1}>
+                  <Tooltip label={show.name ? show.name : show.title}>
+                    <Text w="full" textAlign="left" noOfLines={1}>
+                      {show.name ? show.name : show.title}
+                    </Text>
+                  </Tooltip>
+                  <Flex w="full">
+                    <Text flex="1">
+                      {new Date(
+                        show.release_date
+                          ? show.release_date
+                          : show.first_air_date
+                      ).getFullYear()}
+                    </Text>
+                    <Tag colorScheme="blue">
+                      {show.media_type === 'tv' ? 'TV' : 'Movie'}
+                    </Tag>
+                  </Flex>
+                </VStack>
+              </Link>
+            </GridItem>
+          )
+        );
+      })}
+    </Grid>
+  );
 };
 
 export default SearchResultsPage;
--- src/renderer/pages/TvShows.tsx
+++ src/renderer/pages/TvShows.tsx
@@ -1,8 +1,20 @@
-import { VStack, Heading, Spinner } from '@chakra-ui/react';
+import {
+  VStack,
+  Heading,
+  Spinner,
+  AspectRatio,
+  Flex,
+  Grid,
+  GridItem,
+  Tag,
+  Tooltip,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 import React, { FC, useState } from 'react';
-import { useDiscoverTMDB } from 'renderer/api/tmdb/api';
-import SearchResults from 'renderer/components/SearchResults';
+import { TMDB_IMAGE_BASE_URL, useDiscoverTMDB } from 'renderer/api/tmdb/api';
 import ShowFilter, { FilterOptions } from 'renderer/components/ShowFilter';
+import { Link } from 'react-router-dom';
 
 const TvShows: FC = () => {
   const [options, setOptions] = useState<FilterOptions>({
@@ -31,7 +43,44 @@ const TvShows: FC = () => {
         TV Shows
       </Heading>
       <ShowFilter defaultShowType={options.type} callback={callbackHandler} />
-      <SearchResults data={data} type={options.type} />
+      <Grid
+        templateColumns={{
+          base: 'repeat(2, 1fr)',
+          md: 'repeat(6, 1fr)',
+        }}
+        gap={6}
+        w="full"
+      >
+        {data?.results.map((show) => {
+          return (
+            show.poster_path && (
+              <GridItem key={show.id}>
+                <Link to={`/details/${show.id}?media_type=tv`}>
+                  <AspectRatio ratio={2 / 3}>
+                    <Image
+                      src={`${TMDB_IMAGE_BASE_URL}${show.poster_path}`}
+                      alt={show.name}
+                    />
+                  </AspectRatio>
+                  <VStack mt={1}>
+                    <Tooltip label={show.name}>
+                      <Text w="full" textAlign="left" noOfLines={1}>
+                        {show.name}
+                      </Text>
+                    </Tooltip>
+                    <Flex w="full">
+                      <Text flex="1">
+                        {new Date(show.first_air_date).getFullYear()}
+                      </Text>
+                      <Tag colorScheme="blue">TV</Tag>
+                    </Flex>
+                  </VStack>
+                </Link>
+              </GridItem>
+            )
+          );
+        })}
+      </Grid>
     </VStack>
   );
 };
